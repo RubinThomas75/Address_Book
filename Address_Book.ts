11b2774053a7f142ebbea519148fbf87787089f7
Rubin Thomas <rubinthomas75gmail.com@Rubins-MacBook-Pro.local>
2018-08-14 11:34:14 -0400

    express server is working. Tested against Postman and shows messages. Now need to route to contacts and add more rest functionality

--- Address_Book.ts
+++ Address_Book.ts
@@ -1,3 +1,15 @@
 //Rubin Thomas
 //Typescript Address Book
 //Training Day 2
+
+const express = require('express');
+
+const requestHandle = express();
+
+requestHandle.use((req, res, next) => {
+    res.status(200).json({
+        message: 'working'
+    });
+});
+
+module.exports = requestHandle;
